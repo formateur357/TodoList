8081141606e322382cb32b10f64791df7e835d3b
formateur357 <formateur357@gmail.com>
2022-12-02 14:10:15 +0100

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,20 @@ import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { TaskDetailsComponent } from './components/task-details/task-details.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {path: '', component: TodoListComponent, pathMatch: 'full'},
+      {path: 'todolist', component: TodoListComponent, pathMatch: 'full'},
+      {path: 'todolist/:id', component: TaskDetailsComponent},
+    ]
+  },
   {path: 'login', component: LoginComponent},
-  {path: 'todolist/:id', component: TaskDetailsComponent},
-  {path: 'todolist', component: TodoListComponent},
   {path: '404', component: NotFoundComponent},
-  {path: '', component: TodoListComponent, pathMatch: 'full'},
   {path: '**', redirectTo: '404'}
 ];
 
